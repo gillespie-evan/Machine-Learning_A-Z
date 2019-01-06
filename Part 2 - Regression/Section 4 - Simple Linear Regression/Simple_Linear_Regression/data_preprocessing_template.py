ad080c5ce315b73a09f1c02e0189bd9094a58748
Evan Gillespie <gillespie.evan@gmail.com>
2019-01-06 15:43:21 -0500

    4.27

--- Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/data_preprocessing_template.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jan  6 12:17:50 2019
+Created on Sun Jan  6 15:17:50 2019
 @author: gillespie.evan@gmail.com
 
 Simple Linear Regression Salary Data
@@ -16,6 +16,10 @@ b0 is the Constant
 The constant is where the DV starts on the X axis
 b1 is the slope
 
+Ordinary Least Squares
+
+SUM (y - y^2)^2 -> min
+
 """
 
 import numpy as np
@@ -23,19 +27,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Import dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('Salary_Data.csv')
 
 # independant variables upper bound not inclusive
+# x is a matrix of IVs
 X = dataset.iloc[:, :-1].values
 
 # dependant variables
-y = dataset.iloc[:, 3].values
+# y is the vector
+y = dataset.iloc[:, 1].values
 
 # split into test and train data
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3)
 
-# FEATURE SCALING
+# FEATURE SCALING if the library doesn't take care of it
 """
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
@@ -43,6 +49,11 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 """
 
+ # Fitting SLR to the training set
+ from sklearn.linear_model import LinearRegression
+ regressor = LinearRegression()
+ 
+ regressor.fit(X_train, y_train)
 
 
 
